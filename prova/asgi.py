c57bb7451ab47fe02e9b8c450e2559b9be9292aa
Pão <joaovictorslf@gmail.com>
2022-11-25 22:28:07 -0300

    fix: remove whitenoise and run sh

--- prova/asgi.py
+++ prova/asgi.py
@@ -10,8 +10,6 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
 import os
 
 from django.core.asgi import get_asgi_application
-from whitenoise import WhiteNoise
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'prova.settings')
 application = get_asgi_application()
-application = WhiteNoise(application, root="public/")
--- run.sh
+++
@@ -1,1 +0,0 @@
-uvicorn prova.wsgi:application
