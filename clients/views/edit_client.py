59189fadaeb0311b68f012c4c32f11b2ea578df2
Pão <joaovictorslf@gmail.com>
2022-11-27 22:37:28 -0300

    transaction

--- clients/views/edit_client.py
+++ clients/views/edit_client.py
@@ -1,9 +1,9 @@
-from django.http import HttpRequest, HttpResponse
+from django.http import HttpRequest
 from django.shortcuts import render, redirect
-from clients.forms import create_client
+from django.db import transaction
 
+from clients.forms import create_client
 from clients.models import Client
-from locations.models import CityModel
 
 
 class EditClientView:
@@ -14,11 +14,13 @@ class EditClientView:
         return render(request, 'clients/form.html', {"client": client})
 
     def update(request: HttpRequest, client_id: int):
-        client = Client.objects.select_for_update().filter(id=client_id).first()
+        with transaction.atomic():
+            client = Client.objects.select_for_update().filter(id=client_id).first()
 
-        if request.method.lower() != 'post' or not client:
+            if request.method.lower() != 'post' or not client:
+                return redirect('all_client')
+            form = create_client.CreateClientForm(
+                request.POST, instance=client)
+            form.full_clean()
+            form.save()
             return redirect('all_client')
-        form = create_client.CreateClientForm(request.POST, instance=client)
-        form.full_clean()
-        form.save()
-        return redirect('all_client')
